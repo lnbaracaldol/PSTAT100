37578461a01357170810dcce853a645258f5ddd1
Laura Baracaldo <LauraBaracaldo@users.noreply.github.com>
2023-06-29 12:16:01 -0700

    Adding Lec1

---
+++ docs/Lab0/tests/.ipynb_checkpoints/q2_a-checkpoint.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q2_a',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> type(arr) == np.ndarray\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> arr[4] == 5\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
