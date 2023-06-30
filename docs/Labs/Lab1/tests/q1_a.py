ea33b58357b127ea525f8e6ed10fa974ec4db3b8
Laura Baracaldo <LauraBaracaldo@users.noreply.github.com>
2023-06-30 15:47:53 -0700

    Adding Lab1

---
+++ docs/Labs/Lab1/tests/q1_a.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_a',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> dimensions_baby_names[1] == 5\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
